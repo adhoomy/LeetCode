2554c39feb4e3db66f61570594838a6d5dcbbd7f
adhoomy <adhoomy.mustafa@gmail.com>
2025-02-06 09:45:31 -0500

    Added solution for 704. Binary Search

---
+++ 704. Binary Search/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            m = (l+r)/2
+            if nums[m] == target:
+                return m
+            if target < nums[m]:
+                r = m-1
+            if target >  nums[m]:
+                l = m+1
+        return -1
